17aa629bbc7916f2521cab9014430615a57e48aa
Matthew <55415791+BergaDev@users.noreply.github.com>
2022-05-08 18:32:38 +1000

    Test cam working

---
+++ project name/Assets/Code/FirstLevelCollision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //initalise variables
+        int lives = 0; //lives left, decreases on character death
+        int time = 0; //counts time
+        int speedmult = 0; //multiplies speed
+        int level = 0; //updates each level
+        int death = 0; //updates each time you die
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
