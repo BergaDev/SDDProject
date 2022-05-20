0dd46ac1e13ce5d0b14127187ffd23fee32d150f
Matthew <55415791+BergaDev@users.noreply.github.com>
2022-05-21 09:57:39 +1000

    Added new scenes & added comments around how to use build order
    Title

--- Milner Kong/Assets/Scripts/GameManager.cs
+++ Milner Kong/Assets/Scripts/GameManager.cs
@@ -3,7 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private int level;
+    private int level = 4;
+    //Update this number with the first level of the game from build order, this fixes the infinte loop that can occur when loading the LoadFirst scene
+    
     private int lives;
     private int score;
     //public int CorrectSceneLoad;
@@ -20,7 +22,7 @@ public class GameManager : MonoBehaviour
         lives = 3;
         score = 0;
 
-        LoadLevel(2);
+        LoadLevel(4);
         Debug.Log("Loading LevelOrder " + level);
         //Update this number to match load order in build settings, if not set to the right number a wrong level will be loaded instead
         //If LoadFirst is the number selected the game will get stuck on this level, this caused signicant problems the first time I was building the project
